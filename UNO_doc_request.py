faa7b505cbae4a0ecdeea8a4738925142e508533
HarborUser <118377981+HarborUser@users.noreply.github.com>
2024-02-06 19:44:17 -0600

    Add files via upload

--- UNO_doc_request.py
+++ UNO_doc_request.py
@@ -1,75 +1,47 @@
 import re
-
-
+from unowebsite import *
 import requests
 from bs4 import BeautifulSoup
 from requests.exceptions import RequestException
 from html.parser import HTMLParser
 import time
 
-
 # Replace 'your_url_here' with the actual URL you want to scrape
 link_list = []
-pdf_list =[]
 
 
-url = 'https://www.unomaha.edu/college-of-information-science-and-technology/'
-while url == 'https://www.unomaha.edu/college-of-information-science-and-technology/':
-    try:
-        response = requests.get(url, timeout=5)
-        break
-    except:
-        print("Connection refused by the server..")
-        print("Let me sleep for 5 seconds")
-        print("ZZzzzz...")
-        time.sleep(5)
-        print("Was a nice sleep, now let me continue...")
-        continue
+extended_url = []
 
-soup = BeautifulSoup(response.text, 'html.parser')
 
-for link in soup.find_all('a', attrs={'href':re.compile(("https://www.unomaha.edu/college-of-information-science-and-technology/"))}):
+with open("UNO_link_new_dataset4.txt", "w", encoding='utf-8') as f:
 
-    href = link.get('href')
-    text = link.text
-    link_list.append(href)
-    #for link2 in soup.select("a[href$='.pdf']"):
-        #link_list.append(link2)
-
-    for link2 in soup.findAll('a',attrs={'href': re.compile(".pdf")}):
-        href = link2.get('href')
-        link_list.append(href)
-        pdf_list.append(href)
-
-print(link_list)
-
-with open("UNO_dataset.txt_master2_1", "w", encoding='utf-8') as f:
-
-    for i in range(len(link_list)):
-        new_url = link_list[i]
+    for i in range(len(modified_list)):#link_list
+        
+        new_url = modified_list[i]
 
         responses = requests.get(new_url)
         soup = BeautifulSoup(responses.text, 'html.parser')
 
         i += 1
-        #for data in soup.findAll(["h3","p","li","h6"]):
-        for dat2 in soup.find_all('div', attrs={'id':'main-content'}):
+        #"a", "h3", "href", "span"
 
-            data = dat2.get_text()
+        topmost_id = soup.find('div', id='template-a')
+    
+        if topmost_id:
 
-            for link in soup.find_All('a', attrs={'href': re.compile(".pdf")}):
-                href = link.get("href")
+            for tag in topmost_id.find_all(["p","li","h2","a"]):
+                text = tag.get_text(strip=True)
+            
+                f.write(f"{text}\n")
+        # for data in soup.findAll(["h3","p","a","li"]):
+        #     #for x in soup.find("div", attrs="col").findChildren("p"):
+        #     href = data.get('href')
+        #     extended_url.append(href)
+            
+        #     data1 = data.text
+        #     f.write(f"{new_url}")
+           
+        #     f.write(f' {data1} \n')
+        #     #print(f'{data} link: {href}')
 
-
-                f.write(f"{data}: {href}")
-
-
-
-
-
-
-
-
-
-            #print(f'{data} link: {href}')
 
